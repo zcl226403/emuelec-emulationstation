92127d3eee157a54da36f1d5cfaeca707b4a262a
zcl226403 <837168573@qq.com>
2021-10-03 19:44:37 +0800

    Update GuiMenu.cpp

--- es-app/src/guis/GuiMenu.cpp
+++ es-app/src/guis/GuiMenu.cpp
@@ -603,13 +603,22 @@ if (UIModeController::getInstance()->isUIModeFull()) //备份
 					
 					//判断是否更新
 					FILE *fp;
-
+    				if ((fp=fopen("/storage/system/version/version","r"))==NULL)
+    				{
+    					mWindow->pushGui(new GuiMsgBox(mWindow, _("Is the latest version, no need to update."), _("OK"), nullptr));
+						return;
+    				}
+    				else
+    				{
+    					mWindow->pushGui(new GuiMsgBox(mWindow, _("Have a updated version, please download the firmware, \nwe provide the firmware into the roms/update/directory, \nand then update."), _("OK"), nullptr));
+    					fclose(fp);
+    				}
 
 					//判断是否有固件
     				if ((fp=fopen("/storage/roms/update/update.date","r"))==NULL)//判断文件是否为空
     				{
     					mWindow->pushGui(new GuiMsgBox(mWindow, _("You didn't put in the firmware. Please put the latest firmware (update.date) file provided by us into the  roms/update/  folder."), _("OK"), nullptr));
-						fclose(fp);
+						
 						return;
     				}
 
