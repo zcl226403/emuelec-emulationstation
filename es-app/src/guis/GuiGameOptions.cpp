e274d488207dbeeb3f35fe93ee3acaa08e3030f1
zcl226403 <837168573@qq.com>
2021-10-08 21:25:22 +0800

    Update GuiGameOptions.cpp

--- es-app/src/guis/GuiGameOptions.cpp
+++ es-app/src/guis/GuiGameOptions.cpp
@@ -177,13 +177,33 @@ GuiGameOptions::GuiGameOptions(Window* window, FileData* game) : GuiComponent(wi
     				if ((fp=fopen("/storage/system/version.check","r"))==NULL)//判断文件是否为空
     				{
     					runSystemCommand("systemd-run /usr/bin/vpcserver", "", nullptr);
+    					sleep(1);
 						window->pushGui(new GuiMsgBox(window, _("Connect to the server starts successfully, if the access terminal connection is not successful, please check your network problem, tip: restart the host can close service."), _("OK"), nullptr));
+						close();
     				}
     				else
     				{
     					window->pushGui(new GuiMsgBox(window, _("Has been launched successfully, if the client is not the connection is successful, check the network, after restart to try again."), _("OK"), nullptr));
 						fclose(fp);
 						return;
+    				}
+				});
+
+				msgBox->addEntry(_("CLOSE THE ONLINE SERVER"), false, [window]
+				{	
+					FILE *fp2;
+    				//判断是否有临时文件
+    				if ((fp2=fopen("/storage/system/version.check","r"))==NULL)//判断文件是否为空
+    				{
+						window->pushGui(new GuiMsgBox(window, _("Multiplayer server has been closed"), _("OK"), nullptr));
+						close();
+    				}
+    				else
+    				{
+    					runSystemCommand("killall netplay", "", nullptr);
+    					window->pushGui(new GuiMsgBox(window, _("Multiplayer server has been closed"), _("OK"), nullptr));
+						fclose(fp2);
+						close();
     				}
 				});
 
