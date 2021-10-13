59c2ebfa3d702f558562906e1c51aa8f3917639f
zcl226403 <837168573@qq.com>
2021-10-13 17:56:37 +0800

    Update FileData.cpp

--- es-app/src/FileData.cpp
+++ es-app/src/FileData.cpp
@@ -455,9 +455,11 @@ bool FileData::launchGame(Window* window, LaunchGameOptions options)
 		
 		if (!options.netplayClientPassword.empty())
 			pass = " -netplaypass " + options.netplayClientPassword;
-		
+
 		std::string netplayip;
+		std::string netplaybiaozhi;
 		netplayip = "139.9.249.246";
+		netplaybiaozhi = "[JXZ]";
 
 #if WIN32
 		if (Utils::String::toLower(command).find("retroarch.exe") != std::string::npos)
@@ -469,7 +471,7 @@ bool FileData::launchGame(Window* window, LaunchGameOptions options)
 		if (localip.empty())
 			command = Utils::String::replace(command, "%NETPLAY%", "--connect " + netplayip + " --port " + std::to_string(options.port) + " --nick " + SystemConf::getInstance()->get("global.netplay.nickname"));
 		else
-			command = Utils::String::replace(command, "%NETPLAY%", "--connect " + SystemConf::getInstance()->get("global.netplay.localip") + " --port " + SystemConf::getInstance()->get("global.netplay.clineport") + " --nick " + SystemConf::getInstance()->get("global.netplay.nickname"));
+			command = Utils::String::replace(command, "%NETPLAY%", "--connect " + SystemConf::getInstance()->get("global.netplay.localip") + " --port " + SystemConf::getInstance()->get("global.netplay.clineport") + " --nick " + SystemConf::getInstance()->get("global.netplay.nickname") + netplaybiaozhi);
 #else
 		command = Utils::String::replace(command, "%NETPLAY%", "-netplaymode " + mode + " -netplayport " + std::to_string(options.port) + " -netplayip " + SystemConf::getInstance()->get("global.netplay.ip") + pass);
 
