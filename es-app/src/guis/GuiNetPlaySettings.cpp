99c949b0abe4e4c79921e9fd976cbb9e31a83b03
zcl226403 <837168573@qq.com>
2022-11-25 14:44:49 +0800

    Update GuiNetPlaySettings.cpp

--- es-app/src/guis/GuiNetPlaySettings.cpp
+++ es-app/src/guis/GuiNetPlaySettings.cpp
@@ -44,7 +44,7 @@ if (UIModeController::getInstance()->isUIModeFull())
 //    netplay_ifs = "/usr/config/EE_VERSION";
 //    netplay_ip = "121212";
 //    addWithLabel(_("NETPLAY IP"), std::make_shared<string>("2222"));
-    addWithLabel(_("NETPLAY IP"), "11.11.11.11", false);
+    addWithLabel(_("NETPLAY IP"), "11.11.11.11");
 
 	addInputTextRow(_("NICKNAME"), "global.netplay.nickname", false);
 if (UIModeController::getInstance()->isUIModeFull())
