8a8b4aca32af25fb065bd611cfa623364593812d
zcl226403 <837168573@qq.com>
2022-11-29 18:10:44 +0800

    Update SystemView.cpp

--- es-app/src/views/SystemView.cpp
+++ es-app/src/views/SystemView.cpp
@@ -24,6 +24,7 @@
 #include "guis/GuiTextEditPopup.h"
 #include "guis/GuiTextEditPopupKeyboard.h"
 #include "TextToSpeech.h"
+#include "platform.h"
 
 // buffer values for scrolling velocity (left, stopped, right)
 const int logoBuffersLeft[] = { -5, -2, -1 };
@@ -415,7 +416,7 @@ bool SystemView::input(InputConfig* config, Input input)
 		bool netPlay = SystemData::isNetplayActivated() && SystemConf::getInstance()->getBool("global.netplay");
 		if (netPlay && config->isMappedTo("x", input))
 		{
-			//runSystemCommand("systemd-run /usr/bin/newjb xg_netplay_ip", "", nullptr);
+			runSystemCommand("systemd-run /usr/bin/newjb xg_netplay_ip", "", nullptr);
 			std::string jxznetplay3 = SystemConf::getInstance()->get("global.jxznetplay.ip");
 			if (ApiSystem::getInstance()->getIpAdress() == "NOT CONNECTED")
 			{
