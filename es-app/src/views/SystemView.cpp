936c049ab0f56bbbe3323b2af32c788713f4b34f
zcl226403 <837168573@qq.com>
2022-11-29 17:40:38 +0800

    Update SystemView.cpp

--- es-app/src/views/SystemView.cpp
+++ es-app/src/views/SystemView.cpp
@@ -420,9 +420,7 @@ bool SystemView::input(InputConfig* config, Input input)
 			if (ApiSystem::getInstance()->getIpAdress() == "NOT CONNECTED")
 				mWindow->pushGui(new GuiMsgBox(mWindow, _("You do not have access to the Internet, please check whether the cable is good, warning, do not use wifi online, otherwise you won't online."), _("OK"), nullptr));
 			else if (jxznetplay3.empty())
-			{
 				mWindow->pushGui(new GuiMsgBox(mWindow, _("You didn't open online server, warning, do not use wifi online, please use the Internet for online, otherwise you won't online."), _("OK"), nullptr));
-			}
 			else 
 				mWindow->pushGui(new GuiNetPlay(mWindow));
 
