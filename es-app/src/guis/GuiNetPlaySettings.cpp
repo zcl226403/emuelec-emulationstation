7d9166186407323ccfbc1c35de10e761ec210c49
zcl226403 <837168573@qq.com>
2023-03-06 16:40:50 +0800

    Update GuiNetPlaySettings.cpp

--- es-app/src/guis/GuiNetPlaySettings.cpp
+++ es-app/src/guis/GuiNetPlaySettings.cpp
@@ -69,8 +69,8 @@ if (UIModeController::getInstance()->isUIModeFull())
 	std::shared_ptr<Font> font = theme->Text.font;
 	unsigned int color = theme->Text.color;
 
-	addOptionList(_("LATENCY FRAMES RANGE"), { { _("0") , "0" },{ _("1") , "1" },{ _("2") , "2" },{ _("3") , "3" },{ _("4") , "4" } }, "global.netplay.latency", false);
-	addOptionList(_("MAX CONNECTIONS"), { { _("4") , "4" },{ _("5") , "5" },{ _("6") , "6" },{ _("7") , "7" },{ _("8") , "8" },{ _("9") , "9" },{ _("10") , "10" },{ _("11") , "11" },{ _("12") , "12" },{ _("13") , "13" },{ _("14") , "14" },{ _("15") , "15" } }, "global.netplay.connections", false);
+	addOptionList(_("LATENCY FRAMES RANGE"), _("The higher the value, the smoother the online, and the higher the delay. The default recommended value is 2."), { { _("0") , "0" },{ _("1") , "1" },{ _("2") , "2" },{ _("3") , "3" },{ _("4") , "4" } }, "global.netplay.latency", false);
+	addOptionList(_("MAX CONNECTIONS"), _("The maximum number of people allowed in your room is 4 by default."), { { _("4") , "4" },{ _("5") , "5" },{ _("6") , "6" },{ _("7") , "7" },{ _("8") , "8" },{ _("9") , "9" },{ _("10") , "10" },{ _("11") , "11" },{ _("12") , "12" },{ _("13") , "13" },{ _("14") , "14" },{ _("15") , "15" } }, "global.netplay.connections", false);
 
 	auto NetPlayIP = std::make_shared<TextComponent>(mWindow, SystemConf::getInstance()->get("global.jxznetplay.ip"), font, color);
     addWithLabel(_("NETPLAY IP"), NetPlayIP);
@@ -99,7 +99,7 @@ if (UIModeController::getInstance()->isUIModeFull())
 	});
 	addInputTextRow(_("other PSP IP"), "global.netplay.opspip", false);
 
-	addEntry(_("Map Other IP to PSP"), true, [this] //fuzhi duifang IP gei psp
+	addEntry(_("Map Other IP to PSP"), _("The maximum number of people allowed in your room is 4 by default."), true, [this] //fuzhi duifang IP gei psp
 	{
 		mWindow->pushGui(new GuiMsgBox(mWindow, _("You will map the IP of the other party to the local PSP as the secondary host of the other party. Are you sure you want to map?"), _("YES"),
 			[this] {
